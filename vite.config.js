db1db609ab5797c369e7e324feff24bc1566ac40
ZhaoHou Lin <rodes5292@gmail.com>
2022-10-19 15:02:42 +0800

    網頁部署

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ZZ/',
   plugins: [vue()],
   resolve: {
     alias: {
