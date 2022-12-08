4eaeefeb39b11aeaf3946b5ab62e4d5d7d4c0cf8
ZhaoHou Lin <rodes5292@gmail.com>
2022-12-08 17:18:54 +0800

    deploy

---
+++ assets/Page-4.4b1cc0bf.js
@@ -0,0 +1,1 @@
+import{_ as a,o as s,c as _,b as o,t as c}from"./index.e70b996e.js";const n={class:"page page-4"},p={__name:"Page-4",props:{title:String},setup(e){const t=e;return(r,l)=>(s(),_("div",n,[o("h1",null,c(t.title),1)]))}},d=a(p,[["__scopeId","data-v-e4a15113"]]);export{d as default};
